2ad5c519ce49607605be469f3bb337a336e44221
johnkagga <johnkagga@gmail.com>
2017-10-20 16:46:28 +0300

    Add latest BTC and ETH conversion rates for each of the 20 currencies

--- app/src/main/java/com/johnkagga/jkconverter/utility/Constants.java
+++ app/src/main/java/com/johnkagga/jkconverter/utility/Constants.java
@@ -2,8 +2,9 @@ package com.johnkagga.jkconverter.utility;
 
 
 public class Constants {
-
-
     public static final String COIN_IMAGE_ID = "coin_image";
     public static final String CURRENCY_SYMBOL = "currency_symbol";
+    public static final String CRYPTOCOMPARE_BASE_API = "https://min-api.cryptocompare.com/data/price";
+    public static final String COIN_TYPE = "fsym";
+    public static final String BASE_CURRENCY_TO_CONVERT_TO = "tsyms";
 }
